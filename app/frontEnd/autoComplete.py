438f89de2899d31aa6e34ffb9fe7cb93e91cae45
Sofi-240 <54475550+Sofi-240@users.noreply.github.com>
2023-05-05 08:10:10 +0300

    autoComplete

--- app/frontEnd/autoComplete.py
+++ app/frontEnd/autoComplete.py
@@ -1,35 +1,124 @@
 from app.communication.query import DataQueries
 
 
-class txtNode:
-    def __init__(self, root, sentence, items=None):
-        self.root = root
-        self.sentence = sentence
+class latNode:
+    def __init__(self, sentence='', items=None):
         if items is None:
             items = []
         self.items = items
-        if self.items:
-            self.items.sort()
+        self.sentence = sentence
         self.next = None
+
+
+class wordNode:
+    def __init__(self, TierRoot, sentence):
+        self.TierRoot = TierRoot
+        self.sentence = sentence
+        items = []
+        self._walkTier(self.TierRoot, [self.sentence], items)
+        if items:
+            items.sort()
+        self.next = None
+        self.root = latNode(sentence, items)
+        self.backSpaceBool = False
+
+    def _walkBack(self):
+        if not self.root.next:
+            return
+        self.root = self.root.next
+        return self._walkBack()
+
+    def _walkTier(self, node, sentence, sentence_list):
+        if node.children:
+            for word in node.children:
+                if not sentence[0]:
+                    sentence_new = [word]
+                else:
+                    sentence_new = sentence + [word]
+                if node.children[word].end:
+                    sentence_list.append(' '.join(sentence_new))
+                self._walkTier(node.children[word], sentence_new, sentence_list)
+
+    def keyRelease(self, txt):
+        if self.backSpaceBool:
+            self.backSpaceBool = False
+            return self.root.items
+        if not txt:
+            self._walkBack()
+            return self.root.items
+
+        if len(txt) == len(self.sentence):
+            return self.root.items
+
+        txt = txt.lower()
+        self.sentence = txt
+
+        def mapFunc(e):
+            if len(e) < len(txt):
+                return
+            if e[:len(txt)] == txt:
+                return True
+            return False
+
+        temp = list(filter(mapFunc, self.root.items))
+        prevNode = self.root
+        self.root = latNode(txt, temp)
+        self.root.next = prevNode
+        return self.root.items
+
+    def backSpace(self):
+        if not self.root.next:
+            return False
+        self.root = self.root.next
+        self.sentence = self.root.sentence
+        self.backSpaceBool = True
+        return True
 
 
 class AUTO_complete:
     def __init__(self):
-        self.prev = None
-        self._init(DataQueries('his_project').SymptomsTrie.root)
-        self.forwardCounter = 0
-        self.backwardCounter = 0
+        self.root = wordNode(DataQueries('his_project').SymptomsTrie.root, '')
+        self.spaceBoll = False
 
-    def _init(self, root):
-        sentence_list = []
-        self._walk(root, [''], sentence_list)
-        self.prev = txtNode(root, [''], sentence_list)
+    def _walkBack(self):
+        if not self.root.next:
+            return
+        self.root = self.root.next
+        return self._walkBack()
+
+    def initFrom(self, txt):
+        if not txt:
+            return
+        if self.root.next:
+            self._walkBack()
+
+        wordList = list(txt)
+        prev = ''
+        while wordList:
+            curr = wordList.pop(0)
+            if curr == '':
+                self.space()
+            prev += curr
+            self.keyRelease(prev)
         return
 
-    def space(self, word):
-        node = self.prev.root
-        sentence_list = []
-        word = list(word.lower().split())
+    def initValues(self):
+        return self.root.root.items
+
+    def keyRelease(self, txt):
+        if self.spaceBoll:
+            self.spaceBoll = False
+            return self.root.root.items
+
+        if not txt:
+            self._walkBack()
+            return self.root.root.items
+        ret = self.root.keyRelease(txt)
+        return ret
+
+    def space(self):
+        node = self.root.TierRoot
+        word = list(self.root.sentence.split())
         if not word:
             return
         word = word[-1]
@@ -37,44 +126,17 @@ class AUTO_complete:
             node = node.children[word]
         else:
             return
-        if node.end:
-            sentence_new = self.prev.sentence + [word]
-            return [' '.join(sentence_new[1:])]
-        self._walk(node, self.prev.sentence + [word], sentence_list)
-        temp = self.prev
-        self.prev = txtNode(node, self.prev.sentence + [word], sentence_list)
-        self.prev.next = temp
-        self.backwardCounter = 0
-        self.forwardCounter = 0
+        tempNode = self.root
+        self.root = wordNode(node, self.root.sentence.lower())
+        self.root.next = tempNode
+        self.spaceBoll = True
         return
 
     def backSpace(self):
-        if self.backwardCounter == len(self.prev.sentence[-1]) and len(self.prev.sentence[-1]) != 0:
-            self.prev = self.prev.next
-            self.backwardCounter = 0
+        if self.root.backSpace():
             return
-        self.backwardCounter += 1
+        if not self.root.next:
+            return
+        self.root = self.root.next
         return
 
-    def keyRelease(self, sentence):
-        if not sentence:
-            return []
-        sentence = sentence.lower()
-
-        def mapFunc(e):
-            if len(e) < len(sentence):
-                return
-            if e[:len(sentence)] == sentence:
-                return True
-            return False
-
-        temp = list(filter(mapFunc, self.prev.items))
-        return temp
-
-    def _walk(self, node, sentence, sentence_list):
-        if node.children:
-            for word in node.children:
-                sentence_new = sentence + [word]
-                if node.children[word].end:
-                    sentence_list.append(' '.join(sentence_new[1:]))
-                self._walk(node.children[word], sentence_new, sentence_list)
